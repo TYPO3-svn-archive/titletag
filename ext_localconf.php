761eb3ce4c927b8a759ab48698cf209b6b1dca42
christian.futterlieb <christian.futterlieb@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-01 22:54:39 +0000

    - only register the hook in FE mode
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/titletag/trunk@70607 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -35,9 +35,11 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 // Register hooks
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess']['tx_titletag'] = 'EXT:titletag/Classes/Utility/TitletagUtility.php:&Aaw\Titletag\Utility\TitletagUtility->renderTitle';
-// $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['tx_titletag'] = 'EXT:titletag/class.tx_titletag.php:&tx_titletag->substituteIntInc';
-// $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output']['tx_titletag'] = 'EXT:titletag/class.tx_titletag.php:&tx_titletag->modifyTitle';
-// $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['pageLoadedFromCache']['tx_titletag'] = 'EXT:titletag/class.tx_titletag.php:&tx_titletag->pageLoadedFromCache';
-// $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-cached']['tx_titletag'] = 'EXT:titletag/class.tx_titletag.php:&tx_titletag->tsfeSaveCache';
+if(TYPO3_MODE == 'FE') {
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess']['tx_titletag'] = 'EXT:titletag/Classes/Utility/TitletagUtility.php:&Aaw\Titletag\Utility\TitletagUtility->renderTitle';
+    // $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['tx_titletag'] = 'EXT:titletag/class.tx_titletag.php:&tx_titletag->substituteIntInc';
+    // $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output']['tx_titletag'] = 'EXT:titletag/class.tx_titletag.php:&tx_titletag->modifyTitle';
+    // $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['pageLoadedFromCache']['tx_titletag'] = 'EXT:titletag/class.tx_titletag.php:&tx_titletag->pageLoadedFromCache';
+    // $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-cached']['tx_titletag'] = 'EXT:titletag/class.tx_titletag.php:&tx_titletag->tsfeSaveCache';
+}
 ?>
