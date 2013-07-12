e78a3927001b9bf31fb89f4592420a9b4a9df405
christian.futterlieb <christian.futterlieb@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-12 15:09:10 +0000

    - released version 0.3.1 to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/titletag/trunk@77499 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "titletag".
  *
- * Auto generated 12-07-2013 11:16
+ * Auto generated 12-07-2013 17:06
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => 'Agentur am Wasser | Maeder & Partner AG',
-	'version' => '0.3.0',
+	'version' => '0.3.1',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-6.1.99',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"Changelog";s:4:"44f0";s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"32e4";s:14:"ext_tables.php";s:4:"417d";s:35:"Classes/Utility/TitletagUtility.php";s:4:"210f";s:43:"Classes/Utility/TitletagUtilityAbstract.php";s:4:"2d3b";s:37:"Classes/Utility/TitletagUtilityV4.php";s:4:"cbb6";s:38:"Configuration/TypoScript/constants.txt";s:4:"520b";s:34:"Configuration/TypoScript/setup.txt";s:4:"7fc2";s:14:"doc/manual.sxw";s:4:"3579";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"Changelog";s:4:"d752";s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"32e4";s:14:"ext_tables.php";s:4:"417d";s:35:"Classes/Utility/TitletagUtility.php";s:4:"210f";s:43:"Classes/Utility/TitletagUtilityAbstract.php";s:4:"2d3b";s:37:"Classes/Utility/TitletagUtilityV4.php";s:4:"cbb6";s:38:"Configuration/TypoScript/constants.txt";s:4:"520b";s:34:"Configuration/TypoScript/setup.txt";s:4:"7fc2";s:14:"doc/manual.sxw";s:4:"3579";}',
 	'suggests' => array(
 	),
 );
