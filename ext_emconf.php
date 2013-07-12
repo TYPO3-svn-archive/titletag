89f1759bee084168bc71a80e8436d6e237a6c2cc
christian.futterlieb <christian.futterlieb@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-12 09:17:07 +0000

    - released version 0.3.0 to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/titletag/trunk@77494 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "titletag".
-#
-# Auto generated 25-02-2013 13:32
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "titletag".
+ *
+ * Auto generated 12-07-2013 11:16
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Title Tag Configuration',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => 'Agentur am Wasser | Maeder & Partner AG',
-	'version' => '0.2.0',
+	'version' => '0.3.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5-0.0.0',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"Changelog";s:4:"1516";s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"4dcf";s:14:"ext_tables.php";s:4:"417d";s:35:"Classes/Gateway/TitletagGateway.php";s:4:"0b27";s:35:"Classes/Utility/TitletagUtility.php";s:4:"3530";s:37:"Classes/Utility/TitletagUtilityV4.php";s:4:"f54d";s:38:"Configuration/TypoScript/constants.txt";s:4:"520b";s:34:"Configuration/TypoScript/setup.txt";s:4:"2f40";s:14:"doc/manual.sxw";s:4:"3579";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"Changelog";s:4:"44f0";s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"32e4";s:14:"ext_tables.php";s:4:"417d";s:35:"Classes/Utility/TitletagUtility.php";s:4:"210f";s:43:"Classes/Utility/TitletagUtilityAbstract.php";s:4:"2d3b";s:37:"Classes/Utility/TitletagUtilityV4.php";s:4:"cbb6";s:38:"Configuration/TypoScript/constants.txt";s:4:"520b";s:34:"Configuration/TypoScript/setup.txt";s:4:"7fc2";s:14:"doc/manual.sxw";s:4:"3579";}',
 	'suggests' => array(
 	),
 );
